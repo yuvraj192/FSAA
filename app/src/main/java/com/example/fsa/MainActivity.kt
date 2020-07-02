fcc3ab8196bcddbf933ef490e00e8790aef44dbf
divyansh.gupta <divyanshg809@gmail.com>
2020-07-02 18:44:57 +0530

    hello

--- app/src/main/java/com/example/fsa/MainActivity.kt
+++ app/src/main/java/com/example/fsa/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.fsa
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       sendBtn.setOnClickListener{
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }else {
+                    pickImage();
+                }
+        }
+    }
+private fun pickImage(){
+ val intent = Intent(Intent.ACTION_PICK)
+    intent.type = "image/*"
+    startActivityForResult(intent, IMAGE_PICK_CODE)
+}
+    companion object{
+        private val IMAGE_PICK_CODE = 1000;
+        private val PERMISSION_CODE = 1001;
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImage()
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imageView.setImageURI(data?.data)
+        }
     }
 }
